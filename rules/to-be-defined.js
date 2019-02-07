8479790f89a36c11569b04c6fef3e42cabdc4143
Patrick van Zadel <patrickvanzadel@eleven.nl>
2019-02-07 22:13:43 +0100

    v1.1.0

---
+++ rules/to-be-defined.js
@@ -0,0 +1,27 @@
+module.exports = {
+    rules: {
+        'brace-style': 'off',
+        'callback-return': 'off',
+        'camelcase': 'off',
+        'capitalized-comments': 'off',
+        'comma-dangle': 'off',
+        'comma-spacing': 'off',
+        'computed-property-spacing': 'off',
+        'consistent-this': 'off',
+        'eol-last': 'off',
+        'for-direction': 'off',
+        'func-call-spacing': 'off',
+        'func-name-matching': 'off',
+        'func-names': 'off',
+        'generator-star-spacing': 'off',
+        'getter-return': 'off',
+        'global-require': 'off',
+        'id-blacklist': 'off',
+        'id-length': 'off',
+        'id-match': 'off',
+        'implicit-arrow-linebreak': 'off',
+        'init-declarations': 'off',
+        'jsx-quotes': 'off',
+        'key-spacing': 'off'
+    }
+};
